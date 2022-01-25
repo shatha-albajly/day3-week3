4eeeef0b74cfdfa84896d318aab9ef8e83b31680
Shatha <shathaalbajly@gmail.com>
2022-01-25 00:37:06 -0800

    payment

--- js/checkout.js
+++ js/checkout.js
@@ -1,15 +1,20 @@
 const progressBar = document.getElementById("progress-bar");
 const progressNext = document.getElementById("progress-next");
 const progressPrev = document.getElementById("progress-prev");
+const checkoutCards = document.querySelectorAll(".checkout-card");
 const steps = document.querySelectorAll(".step");
+
+const product_name = document.querySelector(".product-name p");
 let active = 1;
 
 progressNext.addEventListener("click", () => {
   active++;
+
   if (active > steps.length) {
     active = steps.length;
   }
   updateProgress();
+  moveCards();
 });
 
 progressPrev.addEventListener("click", () => {
@@ -18,7 +23,18 @@ progressPrev.addEventListener("click", () => {
     active = 1;
   }
   updateProgress();
+  moveCards();
 });
+
+const moveCards = () => {
+  checkoutCards.forEach(function (checkCard, index) {
+    if (active == index + 1) {
+      checkCard.classList.add("active");
+    } else {
+      checkCard.classList.remove("active");
+    }
+  });
+};
 
 const updateProgress = () => {
   steps.forEach((step, i) => {
@@ -29,13 +45,27 @@ const updateProgress = () => {
     }
   });
   progressBar.style.width = ((active - 1) / (steps.length - 1)) * 100 + "%";
-  console.log(steps.length);
   if (active === 1) {
     progressPrev.disabled = true;
+    progressPrev.style.border = "none";
   } else if (active === steps.length) {
     progressNext.disabled = true;
+    progressNext.style.border = "none";
   } else {
     progressPrev.disabled = false;
     progressNext.disabled = false;
+    progressNext.style.border = "1px solid var(--gray-color)";
+    progressPrev.style.border = "1px solid var(--gray-color)";
   }
 };
+
+// read from basket
+let basket = JSON.parse(localStorage.getItem("basket")); // Parse data from localstorage
+basket.forEach((item) => {
+  console.log(item);
+  console.log(item.elementName);
+  product_name.textContent = item.elementName;
+
+  console.log(item.elementPrice);
+  console.log(item.elementQuantity);
+});
--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,29 @@ const login_model = document.querySelector(".login-model");
 const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
-const exit = document.querySelector(".exit img");
+const exit = document.querySelectorAll(".exit img");
 const timer = document.querySelectorAll(".clock");
 const img_cards = document.querySelectorAll(".img-card");
 const model_container = document.querySelector(".model-container");
 let img = document.querySelector(".model-container img");
 const left_arrow = document.querySelector(".model-container .left-arrow i");
 const right_arrow = document.querySelector(".model-container .right-arrow i");
+const circles = document.querySelectorAll(".circle");
 // search variables
 const search = document.getElementById("search");
 const booksNames = document.querySelectorAll(".product-name h2 a p");
 const products = document.querySelectorAll(".product");
-
+const book_item = document.querySelector(".book-item");
 // add elements to the basket
 const add_to_basket = document.querySelectorAll(".btn-card");
 const badge = document.querySelector(".badge");
 slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`;
 });
+
+// slider
 let counter = 0;
+
 nextBtn.addEventListener("click", function () {
   counter++;
   carousel();
@@ -34,27 +38,45 @@ prevBtn.addEventListener("click", function () {
   carousel();
 });
 
-function carousel() {
-  slides.forEach(function (slide) {
-    slide.style.transform = `translateX(-${counter * 100}%)`;
+circles.forEach((circle, index) => {
+  circle.addEventListener("click", () => {
+    counter = index;
+    circle.classList.add("active");
+    carousel();
+    console.log(index);
   });
+});
+
+function carousel() {
+  if (counter <= 5) {
+    slides.forEach(function (slide) {
+      slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+  }
 }
 // signup and login pop up
-show_signup_model.addEventListener("click", () => {
-  signup_model.style.display = "block";
-  login_model.style.display = "none";
-});
+function show_signup_model_fun() {
+  show_signup_model.addEventListener("click", () => {
+    signup_model.style.display = "block";
+    login_model.style.display = "none";
+  });
+}
 
-show_login_model.addEventListener("click", () => {
-  login_model.style.display = "block";
-  signup_model.style.display = "none";
-});
+function show_login_model_fun() {
+  show_login_model.addEventListener("click", () => {
+    login_model.style.display = "block";
+    signup_model.style.display = "none";
+  });
+}
 
-exit.addEventListener("click", (e) => {
-  console.log(e);
-  signup_model.style.display = "none";
-  login_model.style.display = "none";
-});
+function exit_fun() {
+  exit.forEach(function (exit_btn) {
+    exit_btn.addEventListener("click", () => {
+      signup_model.style.display = "none";
+      login_model.style.display = "none";
+    });
+  });
+}
 
 //timer
 let tempDate = new Date();
@@ -63,61 +85,25 @@ let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 // months are ZERO index based;
 const futureDate = new Date(tempYear, tempMonth, tempDay + 11, 7, 30, 0);
-
-// const year = futureDate.getFullYear();
-const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes();
-
 const futureTime = futureDate.getTime();
 function getRemaindingTime() {
   const today = new Date().getTime();
-
   const t = futureTime - today;
-  // 1s = 1000ms
-  // 1m = 60s
-  // 1hr = 60m
-  // 1d = 24hr
-  // values in miliseconds
   const oneDay = 24 * 60 * 60 * 1000;
-  const oneHour = 60 * 60 * 1000;
-  const oneMinute = 60 * 1000;
-  // calculate all values
   let days = t / oneDay;
   days = Math.floor(days);
-  let hours = Math.floor((t % oneDay) / oneHour);
-  let minutes = Math.floor((t % oneHour) / oneMinute);
-  let seconds = Math.floor((t % oneMinute) / 1000);
-  let print_time = `${hours} : ${minutes} : ${seconds}`;
-
-  let total = "";
-  // set values array
-  const values = [hours, minutes, seconds];
-
-  function format(item) {
-    if (item < 10) {
-      return (item = `0${item}`);
-    }
-    return item;
-  }
-
-  values.forEach(function (item, index) {
-    total += ":" + String(format(values[index]));
-  });
-
   if (t < 0) {
     clearInterval(countdown);
     timer.textContent = `sorry, the time has finished!`;
   }
-  total = total.slice(1, 10);
   timer.forEach((oneTimer) => {
-    oneTimer.textContent = total;
+    oneTimer.textContent = days + " يوم";
   });
 }
 // countdown;
 let countdown = setInterval(getRemaindingTime, 1000);
 //set initial values
 getRemaindingTime();
-//
 
 // // search
 search.addEventListener("keyup", function (e) {
@@ -130,41 +116,108 @@ search.addEventListener("keyup", function (e) {
     }
   });
 });
-let NumberOfElement = 0;
 
 // add to basket
+let NumberOfElement = 0;
+
 products.forEach((product) => {
+  console.log(product);
+
   to_basket = product.querySelector(".btn-card");
   to_basket.addEventListener("click", function (add) {
-    //Add item to when click AddBtn localStorage
     add.preventDefault(); // Avoid default action.
 
     let basket = JSON.parse(localStorage.getItem("basket")); // Parse data from localstorage
 
-    // let elementimageUrl = element.imageUrl; // element.imageUrl is a part of backend data received from JSON file
-    // let elementId = element._id; // element._id is a part of backend data received from JSON file
-    // let elementName = element.name; // element.name is a part of backend data received from JSON file
-    // let elementPrice = element.price; // element.price is a part of backend data received from JSON file
-    let elementQuantity = 1;
-
     if (!basket) {
       basket = [];
     }
+    var elementQuantity = 1;
 
     let elementName = product.querySelector(".product-name h2 a p").textContent;
+    let elementPrice = product.querySelector(".price-box .price p").textContent;
+    console.log(basket.length);
+
     NumberOfElement = NumberOfElement + 1;
     // find the index of the item if already in basket
     const itemIndexInBasket = basket.findIndex(
       (basketEntry) => basketEntry.elementName === elementName
     );
+
     if (itemIndexInBasket !== -1) {
       basket[itemIndexInBasket].elementQuantity++;
-      badge.textContent = NumberOfElement;
+      badge.textContent = basket.length;
     } else {
       basket.push({
         elementName,
+        elementPrice,
+        elementQuantity,
       }); // Push not existing data to localstorage
     }
     localStorage.setItem("basket", JSON.stringify(basket));
   });
 });
+
+// //timer
+// let tempDate = new Date();
+// let tempYear = tempDate.getFullYear();
+// let tempMonth = tempDate.getMonth();
+// let tempDay = tempDate.getDate();
+// // months are ZERO index based;
+// const futureDate = new Date(tempYear, tempMonth, tempDay + 11, 7, 30, 0);
+
+// // const year = futureDate.getFullYear();
+// // const hours = futureDate.getHours();
+// // const minutes = futureDate.getMinutes();
+
+// const futureTime = futureDate.getTime();
+// function getRemaindingTime() {
+//   const today = new Date().getTime();
+
+//   const t = futureTime - today;
+//   // 1s = 1000ms
+//   // 1m = 60s
+//   // 1hr = 60m
+//   // 1d = 24hr
+//   // values in miliseconds
+//   const oneDay = 24 * 60 * 60 * 1000;
+//   // const oneHour = 60 * 60 * 1000;
+//   // const oneMinute = 60 * 1000;
+//   // calculate all values
+//   let days = t / oneDay;
+//   days = Math.floor(days);
+//   // let hours = Math.floor((t % oneDay) / oneHour);
+//   // let minutes = Math.floor((t % oneHour) / oneMinute);
+//   // let seconds = Math.floor((t % oneMinute) / 1000);
+//   // let print_time = `${hours} : ${minutes} : ${seconds}`;
+
+//   // let total = "";
+//   // // set values array
+//   // const values = [hours, minutes, seconds];
+
+//   // function format(item) {
+//   //   if (item < 10) {
+//   //     return (item = `0${item}`);
+//   //   }
+//   //   return item;
+//   // }
+
+//   // values.forEach(function (item, index) {
+//   //   total += ":" + String(format(values[index]));
+//   // });
+
+//   if (t < 0) {
+//     clearInterval(countdown);
+//     timer.textContent = `sorry, the time has finished!`;
+//   }
+//   // total = total.slice(1, 10);
+//   timer.forEach((oneTimer) => {
+//     // oneTimer.textContent = total;
+//     oneTimer.textContent = days + " يوم";
+//   });
+// }
+// // countdown;
+// let countdown = setInterval(getRemaindingTime, 1000);
+// //set initial values
+// getRemaindingTime();
+// //
